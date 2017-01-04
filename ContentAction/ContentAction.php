98a73563d1354b7adff7305243a2cf59281d1a71
Timo Tijhof <krinklemail@gmail.com>
2017-01-03 18:03:33 -0800

    Improve ContentAction example
    Follows-up 15d56c7.
    
    * Call parent::show() so that headers and page title are set,
      which all core actions do and most custom actions should as well.
    * Disable subtitle description (not usually used in core either).
    * Move hook to a separate hooks file.
    * Fix incorrect comment about "SkinTemplateContentActions" hook.
    
    Change-Id: I2800a6d4b5082f4894e06813c29642341adea021

--- ContentAction/ContentAction.php
+++ ContentAction/ContentAction.php
@@ -1,7 +1,6 @@
 <?php
 /**
- * An extension that demonstrates how to use the SkinTemplateContentActions
- * hook to add a new content action
+ * An extension that demonstrates how to create a new page action.
  *
  * @author Ævar Arnfjörð Bjarmason <avarab@gmail.com>
  * @copyright Copyright © 2005, Ævar Arnfjörð Bjarmason
@@ -9,28 +8,24 @@
  */
 
 class ContentAction extends FormlessAction {
-	public static function onSkinTemplateNavigation( $skin, &$content_actions ) {
-		$action = $skin->getRequest()->getText( 'action' );
-
-		if ( $skin->getTitle()->getNamespace() != NS_SPECIAL ) {
-			$content_actions['actions']['myact'] = array(
-				'class' => $action === 'myact' ? 'selected' : false,
-				'text' => wfMessage( 'contentaction-myact' )->text(),
-				'href' => $skin->getTitle()->getLocalUrl( 'action=myact' )
-			);
-		}
-
-		return true;
-	}
 	public function getName() {
 		return 'myact';
 	}
+
+	protected function getDescription() {
+		// Disable subtitle under page heading
+		return '';
+	}
+
 	public function onView() {
+		return null;
 	}
 
 	public function show() {
+		parent::show();
+
 		$this->getContext()->getOutput()->addWikiText(
-			'The page name is ' . $this->getTitle()->getText() . ' and you are ' . $this->getUser()->getName()
+			'This is a custom action for page [[' . $this->getTitle()->getText() . ']].'
 		);
 	}
 
--- examples.php
+++ examples.php
@@ -3,6 +3,6 @@
 // Load via extension.json
 wfLoadExtension( 'examples/ContentAction' );
 
-require_once __DIR__ . "/DataPages/DataPages.php";
+require_once __DIR__ . '/DataPages/DataPages.php';
 
-require_once __DIR__ . "/Example/Example.php";
+require_once __DIR__ . '/Example/Example.php';
