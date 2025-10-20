71217f09b32ab87e7b1a4d34d992dc2a07807e10
Peter Hedenskog <phedenskog@wikimedia.org>
2025-10-20 14:52:55 +0200

    selenium: Upgrade to wdio-mediawiki 6.0.0
    This change remove the use of mwbot.
    
    Bug: T407738
    Change-Id: I2beaf415211170fee32403e72b85dff589081d03

--- tests/selenium/specs/api.js
+++ tests/selenium/specs/api.js
@@ -1,27 +1,27 @@
 // this is just a sample on how to create a user using the API
 // the test helps us know that the API functionality works in
 // core when we release a new wdio-mediawiki package
-import { createAccount, mwbot } from 'wdio-mediawiki/Api.js';
+import { createApiClient } from 'wdio-mediawiki/Api.js';
 import { getTestString } from 'wdio-mediawiki/Util.js';
 
 describe( 'api', () => {
 
-	let bot;
+	let api;
 
 	before( async () => {
-		bot = await mwbot();
+		api = await createApiClient();
 	} );
 
 	it( 'can create a user', async () => {
 		const username = getTestString( 'User-' );
 		const password = getTestString();
-		const result = await createAccount( bot, username, password );
+		const result = await api.createAccount( username, password );
 		expect( result.createaccount.status ).toEqual( 'PASS' );
 	} );
 
 	it( 'can read a page', async () => {
 		const pageName = 'Main Page';
-		const response = await bot.read( pageName );
+		const response = await api.read( pageName );
 		// Page id below 0 means the page do not exixts
 		expect( Object.values( response.query.pages )[ 0 ].pageid ).toBeGreaterThan( 0 );
 	} );
